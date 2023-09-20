e999066a3ec0391298adb3bc7c01c49195ceb998
Adriel Lazaro <adriellazaro23@gmail.com>
2023-09-20 19:36:02 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
-from random import choice
+import random
 nome1 = str(input('Primeiro nome: '))
 nome2 = str(input("Segundo nome: "))
 nome3 = str(input("Terceiro nome: "))
 nome4 = str(input("Quarto nome: "))
 lista = [nome1,nome2,nome3,nome4]
-escolhido = choice(lista)
+escolhido = random.choice(lista)
 print('O escolhido foi {}'.format(escolhido))
